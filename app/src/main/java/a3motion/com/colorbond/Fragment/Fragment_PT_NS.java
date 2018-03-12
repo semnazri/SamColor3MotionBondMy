d2eeae2077f08d1c49f575dc9e23d7db3b155e01
samnazri <mr.shanky08@gmail.com>
2018-03-13 00:41:41 +0700

    tinggal rapihin dialog join event set param, dari adapter, projectHistory menunggu API

--- app/src/main/java/a3motion/com/colorbond/Fragment/Fragment_PT_NS.java
+++ app/src/main/java/a3motion/com/colorbond/Fragment/Fragment_PT_NS.java
@@ -41,20 +41,20 @@ public class Fragment_PT_NS extends Fragment {
         view = inflater.inflate(R.layout.fragment_pt_ns,container,false);
         img_nav = view.findViewById(R.id.img_tolbar);
         txt_title = view.findViewById(R.id.txt_title_page);
-        txt_title.setText("PT NS BLUESCOPE Indonesia");
+        txt_title.setText("PT NS BLUESCOPE INDONESIA");
 
         prefsprivate = getActivity().getSharedPreferences(PREFS_PRIVATE, Context.MODE_PRIVATE);
         userid = prefsprivate.getString(BlueScoopPreferences.mem_type, "1");
 
         if (userid.equals("1")) {
             MainActivity.mToolbar.setVisibility(View.GONE);
-            MainActivity.title_page.setText("PT NS BLUESCOPE Indonesia");
+            MainActivity.title_page.setText("PT NS BLUESCOPE INDONESIA");
             MainActivity.img_title.setVisibility(View.GONE);
             MainActivity.title_page.setVisibility(View.VISIBLE);
 
         } else {
             MainActivity_owner.mToolbar.setVisibility(View.GONE);
-            MainActivity_owner.title_page.setText("PT NS BLUESCOPE Indonesia");
+            MainActivity_owner.title_page.setText("PT NS BLUESCOPE INDONESIA ");
             MainActivity_owner.img_title.setVisibility(View.VISIBLE);
             MainActivity_owner.title_page.setVisibility(View.GONE);
         }
